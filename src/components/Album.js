272dd4e3ef22940fb305a977ccdf467aa0a8b84c
TERRENCE WHITTAKER <terr.whittakerjr@gmail.com>
2017-01-12 15:48:15 -0500

    Function calcs Platinum albums

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,7 +10,7 @@ export default class Album extends React.Component {
   render() {
     return (
       <li className="album">
-        <span className={`symbol symbol-${this.props.certification}`} title={typeMap[this.props.certification]}>{this.props.certification}</span>
+        <span className={`symbol symbol-${this.props.certification}`} title={certificationMap[this.props.certification]}>{this.props.certification}</span>
         <span className="year">{this.props.year}</span>
         <span className="city"> {this.props.album}</span>
       </li>
--- src/components/EmceePage.js
+++ src/components/EmceePage.js
@@ -7,9 +7,20 @@ import Album from './Album';
 import emcees from '../data/emcees';
 
 export default class EmceePage extends React.Component {
+
+
+
+
   render() {
     const id = this.props.params.id;
     const emcee = emcees.filter((emcee) => emcee.id === id)[0];
+    const albums = emcee.albums.map((album, i) => album);
+    const platinum = albums.map((album) => { if (album.certification == "P") return album.certification;});
+
+    console.log(platinum)
+
+    console.log(platinum.length)
+
     if (!emcee) {
       return <NotFoundPage/>;
     }
@@ -24,11 +35,11 @@ export default class EmceePage extends React.Component {
             <h2 className="name">{emcee.name}</h2>
           </div>
           <section className="description">
-            {emcee.rapper} is from {emcee.city}, {emcee.state}
-            born in {emcee.birth} (Find out more on <a href={emcee.link} target="_blank">Wikipedia</a>).
+            {emcee.rapper} is from {emcee.city}, {emcee.state} born in {emcee.birth} (Find out more on <a href={emcee.link} target="_blank">Wikipedia</a>).
           </section>
           <section className="albums">
-            <p>Discography includes <strong>{emcee.albums.length}</strong> albums:</p>
+
+            <p>Discography includes <strong>{platinum.length}</strong> certified Platinum albums:</p>
             <ul>{
               emcee.albums.map((album, i) => <Album key={i} {...album}/>)
             }</ul>
